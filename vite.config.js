580c65d7a1962b2034f4b3475c65020ee60ea9c9
Pavel <pavelguest@yandex.ru>
2026-07-03 11:26:22 +0300

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-import { defineConfig } from 'vite'
-import { viteSingleFile } from 'vite-plugin-singlefile'
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   plugins: [viteSingleFile()],
-})
+  build: {
+    outDir: "dist",
+    assetsInlineLimit: 100000000,
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
+  base: "./",
+});
